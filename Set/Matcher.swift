0c1ee0c875be84058f3a36b4b83aff7c45b2bd29
Nura <nura.qasrawi@gmail.com>
2020-10-05 13:33:31 +0300

    matching functionality added

--- Set/Matcher.swift
+++ Set/Matcher.swift
@@ -21,13 +21,32 @@ class Matcher {
             let card = Card()
             cards.append(card)
         }
+        selectedCards.reserveCapacity(3)
     }
     
     func chooseCard (at index: Int) {
-        if !selectedCards.contains(cards[index]),!matchedCards.contains(cards[index]){
+        if  !selectedCards.contains(cards[index]),!matchedCards.contains(cards[index]),selectedCards.count < 3{
+            print("enter")
+
             selectedCards.append(cards[index])
-        }else if !matchedCards.contains(cards[index]),selectedCards.contains(cards[index]){
-            //comparision 
+        }else if !matchedCards.contains(cards[index]){
+            //comparision
+            if selectedCards.count != 0 {
+              let match = selectedCards.filter {
+                    card in return card == cards[index]
+                }
+                if match.count > 0 {
+                    print("nopeee")
+                    deselectCards()
+                } else{
+                    print(" a match")
+
+                }
+            }
         }
+    }
+    
+    func deselectCards(){
+  selectedCards.removeAll()
     }
 }
--- Set/ViewController.swift
+++ Set/ViewController.swift
@@ -100,14 +100,14 @@ class ViewController: UIViewController {
                 color = #colorLiteral(red: 0.2745098174, green: 0.4862745106, blue: 0.1411764771, alpha: 1)
 
             }
+            
             let attributes: [NSAttributedString.Key : Any] = [ // note: type cannot be inferred here
             .strokeWidth : coloring,
             .foregroundColor:color.withAlphaComponent(CGFloat(alpha)),
-            .font: UIFont.systemFont(ofSize: 20)
            ]
             let attribText = NSAttributedString(string: title , attributes: attributes)
             button.setAttributedTitle(attribText, for: UIControl.State.normal)
-           
+            
         }
         
     }
